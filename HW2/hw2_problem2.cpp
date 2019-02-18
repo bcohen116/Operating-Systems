58edcd5ab4ff6457d425413ac1d21a704963bf8e
Ben Cohen <benfish116@yahoo.com>
2019-02-18 14:39:48 -0500

    fixed hw2 problem 2 to use 2 pipes as needed by the requirements

--- HW2/hw2_problem2.cpp
+++ HW2/hw2_problem2.cpp
@@ -2,6 +2,10 @@
 #include <stdio.h>//for printf
 #include <stdlib.h>//for exit
 #include <string>
+#include <cstring>//for strncmp
+#include <fcntl.h>//for pipe
+#include <sys/stat.h>//for pipes
+#include <sys/types.h>//for pipes
 
 using namespace std;
 
@@ -13,22 +17,35 @@ using namespace std;
 /// Display the result in all upper case using another popen
 ///</summary>
 int main(int argc, char* argv[]) {
-	int pipe1[2], pipe2[2];
 	char buffer[256];
 	string resultList;
-	pipe(pipe1);//open pipe1
-	pipe(pipe2);//open pipe2
+	string finalResultList;
+	FILE * upcaseList;
 	
-	FILE * fileList = popen("find . -name \\*.c -print | tr a-z A-Z", "r");//runs unix command to find all files ending in .c in this directory
+	//Technically all we need is this one line, but the problem asks for two pipes
+	//FILE * fileList = popen("find . -name \\*.c -print | tr a-z A-Z", "r");//runs unix command to find all files ending in .c in this directory
+
+	//Since we need two pipes, we will split this up into getting the list, and then upcasing the list
+	//First pipe
+	FILE * fileList = popen("find . -name \\*.c -print", "r");//runs unix command to find all files ending in .c in this directory
 	//read the contents of popen into a buffer, then save to a string
 	while (fgets(buffer, sizeof buffer, fileList) != NULL) {
 		resultList += buffer;
 	}
 	
-	printf("Result: %s", resultList.c_str());
-	//We have all of the files names, now we have to convert them
-	//FILE * uppercaseList = popen(resultList.c_str() + "| tr a-z A-Z", "r");//convert file names to upper case
+	//At this point we have the list of files, now we need to convert them using the second pipe
+	string commandString = "echo \"";//escaping a quote so we can ignore line breaks in resultList
+	commandString += resultList.c_str();
+	commandString += "\" | tr a-z A-Z";
+	//open second pipe
+	upcaseList = popen(commandString.c_str(), "r");//runs unix command to convert the buffer to upcase
+	//copy the results of the upcase to a string
+	while (fgets(buffer, sizeof buffer, upcaseList) != NULL) {
+		finalResultList += buffer;
+	}
 	
+	printf("All files ending in .C in upcase including subdirectories:\n %s \n", finalResultList.c_str());
+	pclose(upcaseList);
 	pclose(fileList);//safely close memory opened earlier in the program
 	return 0;
 }
